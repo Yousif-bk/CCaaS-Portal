be9ef67e9c2706e2d5af4d6204cab2c65eecc388
Yousif <yusuf.babiker0@gmail.com>
2025-05-05 16:26:38 +0400

    update outbound call

--- Scripts/NICE CXone Agent/NICE CXone Agent Outbound Call Test/Script1739861441752.groovy
+++ Scripts/NICE CXone Agent/NICE CXone Agent Outbound Call Test/Script1739861441752.groovy
@@ -1,7 +1,3 @@
-import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.WebDriver
-import com.kms.katalon.core.webui.driver.DriverFactory
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
@@ -15,40 +11,35 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.webui.driver.DriverFactory
 
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.WebDriver
 
 import internal.GlobalVariable as GlobalVariable
 
-
-// Set Chrome options to allow microphone access
+// Configure Chrome options
 ChromeOptions options = new ChromeOptions()
 Map<String, Object> prefs = new HashMap<String, Object>()
-prefs.put("profile.default_content_setting_values.media_stream_mic", 1)  // 1 = allow, 2 = block
-prefs.put("profile.default_content_setting_values.media_stream_camera", 1)
+prefs.put("profile.default_content_setting_values.media_stream_mic", 1)     // Allow microphone
+prefs.put("profile.default_content_setting_values.media_stream_camera", 1)  // Allow camera
 options.setExperimentalOption("prefs", prefs)
+options.addArguments('--use-fake-ui-for-media-stream')
+options.addArguments('--disable-notifications')
 
-WebDriver driver = new ChromeDriver(options)
-DriverFactory.changeWebDriver(driver)
+// Apply Chrome options to Katalon WebUI
+WebDriver driver = DriverFactory.getWebDriver()
+DriverFactory.changeWebDriver(new org.openqa.selenium.chrome.ChromeDriver(options))
 
-// Now continue your test
+// Begin test
 WebUI.navigateToUrl('https://cxagent.nicecxone.com/')
 
 WebUI.setText(findTestObject('Object Repository/Nice CXone Agent OR/Page_Sign In/input_Sign In_username'), '800engagex_aabaker@eand.com')
-
 WebUI.click(findTestObject('Object Repository/Nice CXone Agent OR/Page_Sign In/button_NEXT'))
-
 WebUI.setEncryptedText(findTestObject('Object Repository/Nice CXone Agent OR/Page_Sign In/input_Sign In_password'), 'rBxUql4cWJqz1D4sF7V5lQ==')
-
 WebUI.click(findTestObject('Object Repository/Nice CXone Agent OR/Page_Sign In/button_Sign In'))
 
 WebUI.click(findTestObject('Object Repository/Nice CXone Agent OR/Page_NICE CXone/svg_Dialed number is invalid_MuiSvgIcon-roo_0221d2'))
-
 WebUI.setText(findTestObject('Object Repository/Nice CXone Agent OR/Page_NICE CXone/input__searchField'), '+971554573936')
-
 WebUI.click(findTestObject('Object Repository/Nice CXone Agent OR/Page_NICE CXone/div_Voice Call'))
-
 WebUI.click(findTestObject('Object Repository/Nice CXone Agent OR/Page_NICE CXone/path_Dialed number is invalid_Path_12738'))
 
 WebUI.closeBrowser()
